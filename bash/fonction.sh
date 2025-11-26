b1cf2d5c5edd196ae7746b78c74289e218dcba10
Néo CARDOT-BUTZ <neo.cardot-butz@etu.cyu.fr>
2025-11-26 23:29:18 +0100

    Enhance dependency verification in launch script; improve error handling and user guidance

--- bash/fonction.sh
+++ bash/fonction.sh
@@ -5,8 +5,12 @@ verifDependance() {
 
     for cmd in "${args[@]}"; do
         if ! command -v "$cmd" &> /dev/null; then
-            echo "Erreur : La dépendance <$cmd> n'est pas installée. Veuillez l'installer et réessayer." >&2
-            exit 1
+            printf "Erreur : La dépendance <$cmd> n'est pas installée. Veuillez l'installer et réessayer." >&2
+            printf "Pour compilez sans les dépendances ajoutez '--force'."
+            printf "Exemple : ./launch.sh [arg1] [arg2] --force -> compilation sans dépendance."
+            printf "Attention ! : Certaine fonctionalité ne seront pas présentes."
+            return 1
         fi
     done
+    return 0
 }
--- launch.sh
+++ launch.sh
@@ -4,11 +4,44 @@ source ./bash/cmd.sh
 source ./bash/fonction.sh
 
 #exemple de lancement ./launch.sh [arg1 : cmd] [arg2 : help]
-#gnuplot test.gp
-#on vérifit les dépendances avant le lancement
-verifDependance
 
-if [ "$2" = "--help" ] || [ "$2" = "-h" ];then
+#on vérifit les dépendances avant le lancement
+
+if [ "$1" = "-r" ] || [ "$1" = "--run" ];then
+    if { [ "$2" = "--force" ] || [ "$3" = "--force" ]; } && [ verifDependance ];then
+        echo "Vous compilez avec une ou plusieurs librairies manquantes (mode --force)."
+        echo "Vous n'aurez donc pas accès à tout le code."
+        verif_flag=1 #dépendance non installé, pas acces aux graphique
+    elif [ verifDependance ];then
+        echo "Erreur : Il manque une ou plusieurs dépendances pour compiler."
+        echo "Utilisez l'option --force pour compiler malgré tout."
+        exit 1
+    else
+        verif_flag=0
+    fi
+fi
+
+
+#éxécution du code si les conditions sont réuni
+
+if [ "$1" = "-r" ] || [ "$1" = "--run" ]; then
+    if [ "$#" -gt 1 ] && { [ "$2" != "--force" ] && [ "$3" != "--force" ]; }; then
+        echo "Erreur : L'option '$1' ne doit pas être suivie d'autres arguments (sauf --force)." >&2
+        echo "Utilisation correcte : ./launch.sh -r [--force] ou ./launch.sh --run [--force]" >&2
+        exit 1
+    else
+        make
+    fi
+
+elif [ "$1" = "-c" ] || [ "$1" = "--clean" ];then
+    if [ "$#" -gt 1 ];then
+        echo "Erreur : l'option '$1' ne doit pas etre suivit d'autres arguments."
+        echo "Utilisation correcte : ./launch.sh -c ou ./launch.sh --clean"
+    else
+        make clean
+    fi
+
+elif [ "$2" = "--help" ] || [ "$2" = "-h" ];then
     aide "$1"
     exit 0
 elif [ "$1" = "-h" ] || [ "$1" = "--help" ];then
@@ -24,7 +57,7 @@ elif [ "$1" = "-h" ] || [ "$1" = "--help" ];then
 elif [ "$1" = "--arbo" ] && [ "$#" -eq 1 ];then
     arboraissance
     exit 0
-elif [ "$1" = "histo" ];then
+elif [ "$1" = "histo" ] && [ $verif_flag -eq 0 ];then #non acces aux graphiques si gnuplot n'est pas installé
     if [ "$2" = "max" ];then
     #fonction
     echo "appel de la fonction pour max"
@@ -41,8 +74,9 @@ elif [ "$1" = "leaks" ];then
     else #fonction
     echo "appel de la fonction pour leaks"
     fi
-elif [ "$1" = "-r" ] || [ "$1" = "--run" ] || [ -z "$1" ];then
-    make
-elif [ "$1" = "-c" ] || [ "$1" = "--clean" ];then
-    make clean 
+elif [ -z "$1" ];then
+    echo "Erreur : le programme ne peut pas etre compiler sans option."
+    echo "Ajouter -r ou --run pour compiler."
+    echo "Exemple : ./launch -r [arg2] [arg3] ou ./launch --run [arg2] [arg3]"
 fi
+exit 0
