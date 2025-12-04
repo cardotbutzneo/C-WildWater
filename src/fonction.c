7dc58e803502ff03086b35a50931c395db599dc2
Néo CARDOT-BUTZ <neo.cardot-butz@etu.cyu.fr>
2025-12-04 11:48:40 +0100

    Add error handling functions and improve formatting in fonction.c

--- src/fonction.c
+++ src/fonction.c
@@ -1,1 +1,16 @@
 #include "main.h"
+#include "arbres.h"
+#include "string.h"
+
+void printErrreur(char *format){
+    if (!format) return;
+    char *buffer;
+    snprintf(buffer,strlen(*format),format);
+}
+
+void récupérer_max(pAVL avl,Dictionnaire* dict){
+    if (!avl) return;
+    if (!dict){
+    
+    }
+}
